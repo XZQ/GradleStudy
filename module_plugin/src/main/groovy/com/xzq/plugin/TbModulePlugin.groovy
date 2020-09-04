c7bfe55314b80c06f4a8383176934f188826b5f7
xiazhiqiang <xiazhiqiang1987@gmail.com>
2020-09-04 15:10:03 +0800

    feat: publish plugin

--- module_plugin/src/main/groovy/com/xzq/plugin/TbModulePlugin.groovy
+++ module_plugin/src/main/groovy/com/xzq/plugin/TbModulePlugin.groovy
@@ -3,12 +3,10 @@ package com.xzq.plugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
-class FirstPlugin implements Plugin<Project> {
+class TbModulePlugin implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
-        println "---------------------------FirstPlugin"
-
+        println("this is a groovy plugin...")
     }
-
 }
